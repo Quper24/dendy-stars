0bf076ff7366ceabaa7c629bb90433f51c3a1c90
Quper24 <Quper2405@gmail.com>
2020-06-12 04:33:02 +0200

    add move enemy

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const blocks = document.querySelectorAll('.game div');
 const countRow = Math.sqrt(blocks.length);
 
 let playerIndex = Math.round(blocks.length - (countRow / 2));
+let move = 1;
 
 const indexEnemy = [
     23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
@@ -15,8 +16,35 @@ for (const enemy of indexEnemy) {
 
 const moveEnemys = () => {
     const leftBlockEnemy = indexEnemy[0] % countRow === 0;
-    const rightBlockEnemy = indexEnemy[(indexEnemy.length - 1) % countRow === countRow - 1];
+    const rightBlockEnemy = indexEnemy[indexEnemy.length - 1] % countRow === countRow - 1;
+
+    if ((leftBlockEnemy && move === -1) || (rightBlockEnemy && move === 1)) {
+        move = countRow;
+    } else if (move === countRow) {
+        move = leftBlockEnemy ? 1 : -1;
+    }
+
+    indexEnemy.forEach(index => {
+        blocks[index].classList.remove('enemy')
+    });
+
+    for (let i = 0; i < indexEnemy.length; i++) {
+        indexEnemy[i] += move;
+    }
+
+    indexEnemy.forEach(index => {
+        blocks[index].classList.add('enemy')
+    });
+
+    if (blocks[playerIndex].classList.contains('enemy', 'player')) {
+        alert('Game Over');
+        return;
+    }
+    setTimeout(moveEnemys, 100);
 };
+moveEnemys();
+
+
 
 blocks[playerIndex].classList.add('player');
 
